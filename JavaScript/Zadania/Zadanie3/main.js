a15196e943bfb9ed422828ed198461a4674d4bcb
mariuszrutkowski1989 <mariuszrutkowski1989@gmail.com>
2022-12-27 18:08:51 +0100

    Zad3-done

---
+++ JavaScript/Zadania/Zadanie3/main.js
@@ -0,0 +1,13 @@
+let mojaTablica = [1, 5, 7, 8, 20];
+
+console.log(mojaTablica);
+
+// Poniżej zabawy
+
+// let mojaTablica = [];
+
+// mojaTablica.push(1);
+// mojaTablica.push(56);
+// mojaTablica.push(787);
+
+// console.log(mojaTablica);
