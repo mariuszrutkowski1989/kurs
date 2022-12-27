84449a1db03fbc24a75075c1869578d96a786256
mariuszrutkowski1989 <mariuszrutkowski1989@gmail.com>
2022-12-27 19:53:49 +0100

    Zad7-done

---
+++ JavaScript/Zadania/Zadanie7/suma.js
@@ -0,0 +1,14 @@
+let firstNumber = 21; 
+
+let secondNumber = 37;
+
+function sum(num1, num2) {
+    let result = num1 + num2;
+    return result
+}
+
+// console.log(sum (firstNumber, secondNumber) )
+
+let wynik = sum(firstNumber, secondNumber);
+
+console.log(wynik);
