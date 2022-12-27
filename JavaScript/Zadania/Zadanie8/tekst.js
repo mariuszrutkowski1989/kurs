10dd34d331adf62d334c2ebe5f24a1ba543d5628
mariuszrutkowski1989 <mariuszrutkowski1989@gmail.com>
2022-12-27 20:15:16 +0100

    Zad8-done

---
+++ JavaScript/Zadania/Zadanie8/tekst.js
@@ -0,0 +1,5 @@
+let tekst = document.querySelector("p")
+
+tekst.textContent = 'Im the Scatman Ski-bi dibby dib yo da dub dub Yo da dub dub Ski-bi dibby dib yo da dub dub Yo da dub dub'
+
+console.log(tekst)
