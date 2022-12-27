dba5145bbe3b98d19173f450438121ce0f71f775
mariuszrutkowski1989 <mariuszrutkowski1989@gmail.com>
2022-12-27 18:30:20 +0100

    Zad4-done

---
+++ JavaScript/Zadania/Zadanie4/main.js
@@ -0,0 +1,13 @@
+// let uno = 4;
+// let dos = 6;
+// let tres = 156;
+
+// let array = [uno, dos, tres];
+
+// console.log(array);
+
+let uno = 4, dos = 6, tres = 156;
+
+let array = [uno, dos, tres];
+
+console.log(array)
