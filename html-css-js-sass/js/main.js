796230d2eb8a5ab50a1f9ed3bebefa1c8f4859c5
mariuszrutkowski1989 <mariuszrutkowski1989@gmail.com>
2023-08-02 19:49:04 +0200

    update

---
+++ html-css-js-sass/js/main.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName('mobile-hamburger')[0].addEventListener('click', function(){
+    document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open')
+})
+
+document.getElementsByClassName('mobile-close')[0].addEventListener('click', function(){
+    document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open')
+})
